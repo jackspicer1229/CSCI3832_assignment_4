28377e0dce12f5ae3d9b1acd98865c666d532224
Your Name <jack.spicer@colorado.edu>
2019-05-02 21:49:34 -0600

    working model with 0.54 F1 measure

--- NER.py
+++ NER.py
@@ -1,8 +1,7 @@
-import pandas as pd
-import numpy as np
-import scipy as sp
+import re
 from seqlearn.datasets import load_conll 
 from seqlearn.hmm import MultinomialHMM
+from seqlearn.perceptron import StructuredPerceptron
 from seqlearn.evaluation import bio_f_score
 
 
@@ -13,13 +12,23 @@ from seqlearn.evaluation import bio_f_score
 #Fill this function with features to train seqlearn on.
 #This is where the bulk of the work is to optimize the implementation
 def features(sequence, i):
-	yield "word=" + sequence[i].lower()
-	if sequence[i].isupper():
+	# word = sequence[i].split()[1]
+	word = sequence[i][1]
+	yield "word=" + word.lower()
+	if word.isupper():
 		yield "Uppercase"
 
+	#Check if word i contains a number
+	if re.search('.*\d.*', word):
+		yield "Numeric"
 
+	#Check if word contains a capital letter, but not the first letter
+	if re.search('\b[a-z]+[A-Z]+[a-z]*', word):
+		yield "Capital"
 
-
+	# #Contains hyphen
+	# if re.search('^-{1}$', word):
+	# 	yield "Hyphen"
 
 
 
@@ -27,21 +36,39 @@ def features(sequence, i):
 def main():
 	#Load in training data and pass it through our feature function.
 	#See documentation exact outputs of load_conll
-	samples, labels, sentence_lengths = load_conll("data/gene-trainF18.txt", features)
+	samples, labels, sentence_lengths = load_conll("data/gene-trainF18.txt", features, split=True)
 	
 
-
 	#Train the model with our features
-	hmm = MultinomialHMM()
-	hmm.fit(samples, labels, sentence_lengths)
+	clf = StructuredPerceptron()
+	clf.fit(samples, labels, sentence_lengths)
 
 
 	#Evaluate our model
-	test_samples, test_labels, test_sentence_lengths = load_conll("data/test-run-test-with-keys.txt", features)
-	prediction = hmm.predict(test_samples, test_sentence_lengths)
-	print(bio_f_score(test_labels, prediction))
+	test_samples, test_labels, test_sentence_lengths = load_conll("data/test-run-test-with-keys.txt", features, split=True)
+	prediction = clf.predict(test_samples, test_sentence_lengths)
+	# print(bio_f_score(test_labels, prediction))
+	print(prediction)
 
+	#Output results
+	i = 0
+	j = 1
+	output = []
+	for line in open("data/test-run-test-with-keys.txt"):
+		if(line == "\n"):
+			output.append("\n")
+			j = 1
+			continue
+		else:
+			item = str(j) + "\t" + line.split()[1] + "\t" + prediction[i] + "\n"
+			output.append(item)
+			print(item)
+			i+=1
+			j+=1
 
+	with open('predictions.txt', 'w') as f:
+		for item in output:
+			f.write(item)
 
 
 
